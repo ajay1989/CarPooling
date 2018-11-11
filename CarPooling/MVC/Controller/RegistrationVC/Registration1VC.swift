33c226aa91fe5072672220dfb1ddb6f49fa6a1d4
ArchitRai <architrai108@gmail.com>
2018-11-11 12:36:20 +0530

    Login and registration1 UI
    Archit
    
    Co-Authored-By: architrai <rai_archit60@yahoo.com>

---
+++ CarPooling/MVC/Controller/RegistrationVC/Registration1VC.swift
@@ -0,0 +1,36 @@
+//
+//  Registration1VC.swift
+//  CarPooling
+//
+//  Created by archit rai saxena on 11/11/18.
+//  Copyright © 2018 Ajay Vyas. All rights reserved.
+//
+
+import UIKit
+
+class Registration1VC: UIViewController {
+    
+    @IBOutlet weak var vw_number: UIView! {
+        didSet {
+            vw_number.layer.borderColor = UIColor.gray.cgColor
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
