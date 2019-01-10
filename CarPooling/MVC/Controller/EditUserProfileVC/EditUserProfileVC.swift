7bc41112eee3d1fc4c0a1456dee612e9abf1c3ec
archit rai saxena <architrai108@gmail.com>
2019-01-10 18:24:11 +0530

    Edit user profile VC

---
+++ CarPooling/MVC/Controller/EditUserProfileVC/EditUserProfileVC.swift
@@ -0,0 +1,30 @@
+//
+//  EditUserProfileVC.swift
+//  CarPooling
+//
+//  Created by Archit Rai Saxena on 10/01/19.
+//  Copyright © 2019 Ajay Vyas. All rights reserved.
+//
+
+import UIKit
+
+class EditUserProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
