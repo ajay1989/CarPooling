c47f6731baa8fc64c09c87e8fd856c3f724019e8
archit rai saxena <architrai108@gmail.com>
2019-01-18 16:30:09 +0530

    All UI eith ride storyboard

---
+++ CarPooling/MVC/Controller/RateMyRideVC/DriverMode/DriverMode4.swift
@@ -0,0 +1,30 @@
+//
+//  DriverMode4.swift
+//  CarPooling
+//
+//  Created by Archit Rai Saxena on 18/01/19.
+//  Copyright © 2019 Ajay Vyas. All rights reserved.
+//
+
+import UIKit
+
+class DriverMode4: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
